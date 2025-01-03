7888ca8c5f397e235469a5bf1fa9af49ff05af16
llkui241569 <llkui241569@163.com>
2025-01-03 22:06:45 +0800

    Updated to use theme-chalk

---
+++ chunk-WSSVNT33.js
@@ -0,0 +1,13 @@
+import{o as f,t as h}from"./chunk-LJRBPGSW.js";import"./chunk-JYBRZQRP.js";import{Da as o,Ea as i,Fa as m,I as r,Oa as c,Qb as p,Ua as n,ca as a,la as d,ma as s}from"./chunk-UJIEDWPF.js";var M=(()=>{class t{constructor(){this.quickstartCss=`{
+  "styles": [
+    "node_modules/ng-element-ui/ng-element-ui.css"
+  ]
+}`,this.quickstart=`import { NelButtonModule } from 'ng-element-ui/button';;
+\u2026\u2026
+
+@NgModule({
+  imports: [
+    NelButtonModule
+    \u2026\u2026
+  ]
+})`}static{this.\u0275fac=function(e){return new(e||t)}}static{this.\u0275cmp=d({type:t,selectors:[["app-quickstart"]],standalone:!1,decls:10,vars:2,consts:[[3,"code"]],template:function(e,l){e&1&&(o(0,"h2"),n(1,"\u6837\u5F0F\u5F15\u5165"),i(),o(2,"p"),n(3,"\u5728 `angular.json` \u4E2D\u5F15\u5165 `ng-element-ui.css`"),i(),m(4,"app-code",0),o(5,"h2"),n(6,"\u5FEB\u901F\u4E0A\u624B"),i(),o(7,"p"),n(8,"\u5728 `app.module.ts` \u4E2D\u5F15\u5165"),i(),m(9,"app-code",0)),e&2&&(a(4),c("code",l.quickstartCss),a(5),c("code",l.quickstart))},dependencies:[f],encapsulation:2})}}return t})();var C=[{path:"",component:M}],k=(()=>{class t{static{this.\u0275fac=function(e){return new(e||t)}}static{this.\u0275mod=s({type:t})}static{this.\u0275inj=r({imports:[p.forChild(C),p]})}}return t})();var S=(()=>{class t{static{this.\u0275fac=function(e){return new(e||t)}}static{this.\u0275mod=s({type:t})}static{this.\u0275inj=r({imports:[h,k]})}}return t})();export{S as QuickstartModule};
